1c8eb87a690e7dfa78f1a4deccb4262f65cd0cf7
Vimal Kumar <vmlkmr24@gmail.com>
2025-07-15 16:39:34 +0530

    minor fixes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,10 @@ import chatRoutes from './routes/chatRoutes';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+}));
 app.use(express.json());
 
 app.use('/api', chatRoutes);
